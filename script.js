a212b2eb77c386e879253febb6101b705a5caf1e
burgonyapure <deejay.spory@gmail.com>
2020-04-27 10:59:07 +0200

    New

---
+++ script.js
@@ -0,0 +1,60 @@
+const cards = document.querySelectorAll('.card');
+
+let turned = false;
+let noturn = false;
+
+let firstChoice, secondChoice;
+
+function turn(){
+  if (noturn) return;
+  if (this === firstChoice) return;
+
+  this.classList.add('flip');
+
+  if (!turned) {
+    turned = true;
+    firstChoice = this;
+    return;
+  }
+   secondChoice = this;
+   evaluateChoice();
+}
+
+function evaluateChoice(){
+  let isMatch = firstChoice.dataset.framework === secondChoice.dataset.framework;
+
+  isMatch ? denyTurn() : turnBack();
+}
+
+function denyTurn(){
+  firstChoice.removeEventListener('click', turn);
+  secondChoice.removeEventListener('click', turn);
+
+  resetBoard();
+}
+
+function turnBack(){
+  noturn = true;
+
+  setTimeout(() => {
+    firstChoice.classList.remove('flip');
+    firstChoice.classList.remove('flip');
+
+    resetBoard();
+  }, 1500);
+}
+
+
+function resetBoard() {
+  [turned, noturn] = [false, false];
+  [firstChoice, secondChoice] = [null, null];
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
+
+cards.forEach(card => card.addEventListener('click', turn));
